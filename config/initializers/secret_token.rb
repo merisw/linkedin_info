682ffedacc0fc13bd6d3e8a6186a09d54292f6e6
Meris Williams <merisw@gmail.com>
2013-06-09 16:16:14 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinkedinInfo::Application.config.secret_token = 'f2951c323304ff5a2f2a0a7b812fe6265de1b30ade62214fd22c87f458372da039aac88ac18cc11fb639b8162b7924e541b410ad5cf396ac91f22a801738ec01'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+LinkedinInfo::Application.config.session_store :cookie_store, key: '_linkedin_info_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# LinkedinInfo::Application.config.session_store :active_record_store
